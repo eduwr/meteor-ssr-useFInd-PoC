c0898ef3c1145dd1ad3fadfa0f480ad4cea429cd
eduwr <eduwric@gmail.com>
2022-07-13 15:55:52 -0300

    add useFind

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Hello } from './Hello.jsx';
 import { Info } from './Info.jsx';
+import {useFind} from "meteor/react-meteor-data"
+import {LinksCollection} from "../api/links";
 
-export const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello/>
-    <Info/>
-  </div>
-);
+export const App = () => {
+  const found = useFind(() => LinksCollection.find(), []);
+  console.log(found)
+  return (
+    <div>
+      <h1>Welcome to Meteor!</h1>
+      <Hello/>
+      <Info/>
+    </div>
+  );
+};
 
 export default App
